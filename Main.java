72c14746bb4e300b5c19329b707b409d6448aeca
Zahra <zahrasadeghi003@gmail.com>
2024-06-26 11:21:56 +0430

    git remote add origin https://github.com/ZahraSad70/Java.git git branch -M main git push -u origin main

--- Main.java
+++ Main.java
@@ -8,6 +8,15 @@ public class Main {
         num=reader.nextInt();
         ret=fact(num);
         System.out.println("Answer is : " + ret);
+        int[] factorial={0,1,2,3,4,5,6,7,8,9,10};
+        for (int i=0;i<factorial.length;i++){
+            System.out.println(factorial[i]);
+        }
+        System.out.println("avg is "+avg(factorial));
+
+        String txt="hello i am awsome";
+        System.out.println("the len is "+len(txt));
+
     }
 
     public static int fact(int num){
@@ -16,5 +25,24 @@ public class Main {
         }else{
             return  num * fact(num-1);
         }
+    }
+
+
+    public static int avg(int[] num){
+        int average=0;
+        for (int i=0;i<num.length;i++) {
+            average = num[i] + average;
+
+        }
+        average=average/ (num.length-1);
+        return average;
+    }
+
+    public static int len(String text){
+        String[] txt=null;
+        txt=text.split(" ");
+        String t1=txt[txt.length-1];
+        int sum =t1.length();
+        return sum;
     }
 }
