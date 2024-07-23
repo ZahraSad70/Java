16c6724847f0b04bd179edffff44a99401aed7be
Zahra <zahrasadeghi003@gmail.com>
2024-07-23 12:33:54 +0430

    git remote add origin https://github.com/ZahraSad70/Java.git git branch -M main git push -u origin main

--- AirTrip.java
+++ AirTrip.java
@@ -1,12 +1,12 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
 public class AirTrip {
     private String NamePerson;
     private  int NumAir;  //برای مثال ما فقط دو تا شماره پرواز داریم  شماره پرواز 102 و شماره پرواز 101
-    private static Date DateAir_101=new Date();
-    private static Date DateAir_102=new Date();
+    private static LocalDateTime DateAir_101;
+    private static LocalDateTime DateAir_102;
     private static int statusAir_101=1;  //سه تا مقدار داریم برای این متغیر 1/2/3 به ترتیب از چپ در حال مسافرگیری ، تکمیل شده ، تاخیر
     private static int statusAir_102=1;
 
@@ -47,18 +47,21 @@ public class AirTrip {
             }else if(statusAir_102==3){
                 System.out.println("پرواز تاخیر دارد");
             }
+        }else{
+            System.out.println("این شماره پرواز موجود نیست");
         }
     }
 
     public static void  TimeAir(int NumAir){
+
         if (NumAir==101){
-            SimpleDateFormat Time =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String TimeTrip = Time.format(DateAir_101);
-            System.out.println(TimeTrip);
+            DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            DateAir_101 = LocalDateTime.from(date.parse("2024-07-10 23:00:00"));
+            System.out.println("ساعت پرواز شماره 101" +"\n"+ DateAir_101.toString().replace("T"," "));
         }else if (NumAir==102) {
-            SimpleDateFormat Time =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String TimeTrip = Time.format(DateAir_102);
-            System.out.println(TimeTrip);
+            DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            DateAir_102 = LocalDateTime.from(date.parse("2012-01-10 22:45:00"));
+            System.out.println("ساعت پرواز شماره 102" +"\n"+ DateAir_102.toString().replace("T"," "));
         }else{
             System.out.println("این شماره پرواز موجود نیست");
         }
